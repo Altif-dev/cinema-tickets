4a53f8f3581d5ce0587dd31365b7167bc20c1615
altif <altif_123@outlook.com>
2025-02-07 15:08:52 +0000

    test(red): add test for calling payment service

--- cinema-tickets-javascript/test/ticketService.test.js
+++ cinema-tickets-javascript/test/ticketService.test.js
@@ -1,6 +1,25 @@
 import TicketService from "../src/pairtest/TicketService.js";
 import TicketTypeRequest from "../src/pairtest/lib/TicketTypeRequest.js";
 
-describe('', () => {
+import TicketPaymentService from "../src/thirdparty/paymentgateway/TicketPaymentService.js";
+jest.mock('../src/thirdparty/paymentgateway/TicketPaymentService.js');
 
+describe('Ticket service tests', () => {
+
+    const makePaymentMock = jest.fn();
+    TicketPaymentService.mockImplementation(() => {
+        return {
+            makePayment: makePaymentMock,
+        };
+    });
+
+    it('should make a call to the payment service', () => {
+        //arrange
+        const ticketService = new TicketService();
+        //act
+        ticketService.purchaseTickets(1, [new TicketTypeRequest('ADULT', 1)]);
+        //assert
+        expect(makePaymentMock).toHaveBeenCalled();
+
+    });
 });
