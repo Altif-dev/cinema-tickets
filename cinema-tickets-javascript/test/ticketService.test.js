cac6878047cfb0e7b5c0d719a7b9520837a2b7a1
altif <altif_123@outlook.com>
2025-02-08 18:52:16 +0000

    refactor: organise tests in to separate describe blocks

--- cinema-tickets-javascript/test/ticketService.test.js
+++ cinema-tickets-javascript/test/ticketService.test.js
@@ -1,8 +1,10 @@
 import TicketService from "../src/pairtest/TicketService.js";
 import TicketTypeRequest from "../src/pairtest/lib/TicketTypeRequest.js";
 
-import TicketPaymentService from "../src/thirdparty/paymentgateway/TicketPaymentService.js";
 import InvalidPurchaseException from "../src/pairtest/lib/InvalidPurchaseException.js";
+
+import TicketPaymentService from "../src/thirdparty/paymentgateway/TicketPaymentService.js";
+
 jest.mock('../src/thirdparty/paymentgateway/TicketPaymentService.js');
 
 describe('Ticket service tests', () => {
@@ -20,63 +22,66 @@ describe('Ticket service tests', () => {
         makePaymentMock.mockClear();
     });
 
-    it('should make a call to the payment service', () => {
+    describe('Calling payment service with the correct price', () => {
+        it('should make a call to the payment service', () => {
 
-        const ticketService = new TicketService();
+            const ticketService = new TicketService();
 
-        ticketService.purchaseTickets(1, [new TicketTypeRequest('ADULT', 1)]);
+            ticketService.purchaseTickets(1, [new TicketTypeRequest('ADULT', 1)]);
 
-        expect(makePaymentMock).toHaveBeenCalled();
+            expect(makePaymentMock).toHaveBeenCalled();
+        });
 
-    });
+        it.each([[11, 1, 25], [34, 2, 50]])('should make a call to the payment service with the correct ticket price for a single ticket',
+            (accountId, noOfTickets, expectedCost) => {
 
-    it.each([[11, 1, 25],[34, 2, 50 ]])('should make a call to the payment service with the correct ticket price for a single ticket',
-        (accountId, noOfTickets, expectedCost) => {
+                ticketService.purchaseTickets(accountId, [new TicketTypeRequest('ADULT', noOfTickets)]);
 
-        ticketService.purchaseTickets(accountId, [new TicketTypeRequest('ADULT', noOfTickets)]);
+                expect(makePaymentMock).toHaveBeenCalledWith(accountId, expectedCost);
+            });
 
-        expect(makePaymentMock).toHaveBeenCalledWith(accountId, expectedCost);
-    });
-
-    const ticketRequest1 = [
-        new TicketTypeRequest('ADULT', 1),
-        new TicketTypeRequest('CHILD', 4),
-        new TicketTypeRequest('INFANT', 1)
-    ];
-
-    const ticketRequest2 = [
-        new TicketTypeRequest('ADULT', 2),
-        new TicketTypeRequest('CHILD', 3),
-        new TicketTypeRequest('INFANT', 1)
-    ];
-
-    it.each([[ticketRequest1, 23, 85],[ticketRequest2, 15, 95]])
-    ('should calculate correct ticket price for multiple ticket types in a single transaction',
-        (ticketRequest, accountId, expected) => {
-
-        ticketService.purchaseTickets(accountId, ticketRequest);
-
-        expect(makePaymentMock).toHaveBeenCalledWith(accountId, expected);
-    });
-
-    it('should throw an error if more than 25 tickets are requested', () => {
-        const ticketRequest = [
-            new TicketTypeRequest('CHILD', 16),
-            new TicketTypeRequest('ADULT', 4),
-            new TicketTypeRequest('INFANT', 6),
+        const ticketRequest1 = [
+            new TicketTypeRequest('ADULT', 1),
+            new TicketTypeRequest('CHILD', 4),
+            new TicketTypeRequest('INFANT', 1)
         ];
-        expect(() => {
-            ticketService.purchaseTickets(1 ,ticketRequest)
-        }).toThrow(new InvalidPurchaseException('RangeError: You can only purchase between 1 and 25 tickets per transaction'));
-    });
 
-    it('should throw an error if child or infant tickets are requested without an adult ticket', () => {
-        const ticketRequest = [
-            new TicketTypeRequest('CHILD', 2),
-            new TicketTypeRequest('INFANT', 1),
+        const ticketRequest2 = [
+            new TicketTypeRequest('ADULT', 2),
+            new TicketTypeRequest('CHILD', 3),
+            new TicketTypeRequest('INFANT', 1)
         ];
-        expect(() => {
-            ticketService.purchaseTickets(3 ,ticketRequest)
-        }).toThrow(new InvalidPurchaseException('Error: An adult ticket must purchased with a child or infant ticket'));
+
+        it.each([[ticketRequest1, 23, 85], [ticketRequest2, 15, 95]])
+        ('should calculate correct ticket price for multiple ticket types in a single transaction',
+            (ticketRequest, accountId, expected) => {
+
+                ticketService.purchaseTickets(accountId, ticketRequest);
+
+                expect(makePaymentMock).toHaveBeenCalledWith(accountId, expected);
+            });
+    });
+    
+    describe('Validation tests', () => {
+        it('should throw an error if more than 25 tickets are requested', () => {
+            const ticketRequest = [
+                new TicketTypeRequest('CHILD', 16),
+                new TicketTypeRequest('ADULT', 4),
+                new TicketTypeRequest('INFANT', 6),
+            ];
+            expect(() => {
+                ticketService.purchaseTickets(1, ticketRequest)
+            }).toThrow(new InvalidPurchaseException('RangeError: You can only purchase between 1 and 25 tickets per transaction'));
+        });
+
+        it('should throw an error if child or infant tickets are requested without an adult ticket', () => {
+            const ticketRequest = [
+                new TicketTypeRequest('CHILD', 2),
+                new TicketTypeRequest('INFANT', 1),
+            ];
+            expect(() => {
+                ticketService.purchaseTickets(3, ticketRequest)
+            }).toThrow(new InvalidPurchaseException('Error: An adult ticket must purchased with a child or infant ticket'));
+        });
     });
 });
