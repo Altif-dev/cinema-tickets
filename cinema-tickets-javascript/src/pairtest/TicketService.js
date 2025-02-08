da196b2776588200bcf62732831c290160c87658
altif <altif_123@outlook.com>
2025-02-08 16:12:50 +0000

    refactor: move logic to validate number of tickets in to its own function

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -20,17 +20,9 @@ export default class TicketService {
   purchaseTickets(accountId, ...ticketTypeRequests) {
     const ticketTypeRequestObj = ticketTypeRequests[0];
 
-    let totalNoOfTicketsRequested = 0;
-    ticketTypeRequestObj.forEach(ticket => {
-      totalNoOfTicketsRequested += ticket.getNoOfTickets();
-    })
-
-    if (totalNoOfTicketsRequested > 25 ) {
-      throw new InvalidPurchaseException('You can only purchase between 1 and 25 tickets per transaction')
-    }
+    this.#validateTicketRequest(ticketTypeRequestObj);
 
     const paymentService = new TicketPaymentService();
-
     paymentService.makePayment(accountId, this.#calculateTotalPrice(ticketTypeRequestObj));
   }
 
@@ -47,5 +39,20 @@ export default class TicketService {
       totalPrice += ticket.getNoOfTickets() * this.TICKET_PRICES[ticket.getTicketType()];
     })
     return totalPrice;
+  }
+
+  /**
+   * Validates a given set of ticket type requests
+   * @param ticketTypeRequestObj
+   */
+  #validateTicketRequest(ticketTypeRequestObj) {
+    let totalNoOfTicketsRequested = 0;
+    ticketTypeRequestObj.forEach(ticket => {
+      totalNoOfTicketsRequested += ticket.getNoOfTickets();
+    })
+
+    if (totalNoOfTicketsRequested > 25 ) {
+      throw new InvalidPurchaseException('You can only purchase between 1 and 25 tickets per transaction')
+    }
   }
 }
